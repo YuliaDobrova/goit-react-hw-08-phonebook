1544064d4a7d0e8da1ed646f378a0f472ee43d8e
Yulia Dobrova <dobrova2009@gmail.com>
2021-08-12 13:09:40 +0300

    Updates

---
+++ static/js/7.b35114f4.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[7],{103:function(t,e,o){"use strict";o.r(e);o(0);var n=o(1);e.default=function(){return Object(n.jsxs)(n.Fragment,{children:[Object(n.jsx)("h2",{children:"HomePage"}),Object(n.jsx)("p",{children:"An application that won't let you forget important info \ud83e\udd17 "})]})}}}]);
+//# sourceMappingURL=7.b35114f4.chunk.js.map
